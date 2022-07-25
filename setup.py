193efae13c49a18cd73a16e2b10ffb4b6f20f077
Ishan Jain <ishanjain0421@gmail.com>
2022-07-24 19:11:51 -0700

    Updated version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.2' 
 DESCRIPTION = 'Package to certify variables'
 LONG_DESCRIPTION = 'A python package designed to certify that variable are formatted as intended, and have the right contents'
 
